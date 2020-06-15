21bb2582e8c23eaa77e3198afebd079eaf560bf1
Marcin Sobczyk <msobczyk@redhat.com>
2020-06-15 13:55:02 +0200

    ci: Separate publishing logic in pipelines
    This patch separates pipelines with publishing logic.
    The new pipelines use the same script underneath. What differentiates
    the logic between them is the definition of 'PUBLISH_DIR' variable -
    if it's defined, the publishing logic will apply.
    
    Additionally a 'dnf repomanage' call has been added to remove images
    that are older than 5 days.
    
    Change-Id: I0178c983f449f74f369eea3d395c784330f3909c
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

--- automation/ost-images.sh
+++ automation/ost-images.sh
@@ -2,12 +2,14 @@
 
 BUILDS="rpmbuild"
 EXPORT_DIR="exported-artifacts"
-PUBLISH_DIR="/var/www/html/yum"
+MAX_RPM_AGE="5" # in days
 
 rm -rf "${EXPORT_DIR}"
 mkdir -p "${EXPORT_DIR}"
 
 export LIBGUESTFS_BACKEND=direct
+export LIBGUESTFS_DEBUG=1
+export LIBGUESTFS_TRACE=1
 
 # Ensure /dev/kvm exists, otherwise it will still use
 # direct backend, but without KVM (much slower).
@@ -19,15 +21,23 @@ on_exit() {
     rm -rf "${BUILDS}"
 }
 
+publish_images() {
+    cp -r "${EXPORT_DIR}"/*.rpm "${PUBLISH_DIR}/."
+    dnf repomanage --old --keep "${MAX_RPM_AGE}" "${PUBLISH_DIR}" | xargs -r rm
+    createrepo_c \
+        --update \
+        --retain-old-md-by-age "${MAX_RPM_AGE}d" \
+        --compatibility \
+        "${PUBLISH_DIR}"
+}
+
 trap on_exit EXIT
 
 ./build.sh
 
 find "${BUILDS}" -iname "*.rpm" -exec mv {} "${EXPORT_DIR}/" \;
 find "." -iname "*-pkglist*.txt" -exec mv {} "${EXPORT_DIR}/" \;
-cp -r "${EXPORT_DIR}"/*.rpm "${PUBLISH_DIR}/."
-createrepo_c \
-    --update \
-    --retain-old-md-by-age "5d" \
-    --compatibility \
-    "${PUBLISH_DIR}"
+
+if [ -n "${PUBLISH_DIR}" ]; then
+    publish_images
+fi
