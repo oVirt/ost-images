025bc8f423db0523441f535f894bfc073fac783d
Marcin Sobczyk <msobczyk@redhat.com>
2020-06-25 16:41:26 +0200

    make: Move 'pkglist'-related targets to separate makefile
    To declutter the main makefile we're moving 'pkglist'-related
    targets to separate makefile.
    
    Change-Id: If0f65fafa1c1b779b8e1014d1cd937fad43a1310
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ mk/pkglist.mk
@@ -0,0 +1,11 @@
+%-pkglist.txt: %.qcow2
+	virt-copy-out -a $*.qcow2 $(_PKGLIST_PATH)/$@ .
+
+%-upgrade-pkglist-diff.txt: %-upgrade-pkglist.txt %-base-pkglist.txt
+	$(_DIFF) $*-upgrade-pkglist.txt $*-base-pkglist.txt > $@
+
+%-host-installed-pkglist-diff.txt: %-host-installed-pkglist.txt %-upgrade-pkglist.txt
+	$(_DIFF) $*-host-installed-pkglist.txt $*-upgrade-pkglist.txt > $@
+
+%-engine-installed-pkglist-diff.txt: %-engine-installed-pkglist.txt %-upgrade-pkglist.txt
+	$(_DIFF) $*-engine-installed-pkglist.txt $*-upgrade-pkglist.txt > $@
