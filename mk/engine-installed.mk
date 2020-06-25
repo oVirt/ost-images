a3b939931b561aa67f2fe250a5ae03f55cab5a87
Marcin Sobczyk <msobczyk@redhat.com>
2020-06-25 16:34:54 +0200

    make: Move '{engine,host}-installed' targets to separate makefiles
    To declutter the main makefile we're moving definitions of the targets
    for '{engine,host}-installed' layers to separate sub-makefiles.
    
    Change-Id: I2881ba848c7e77445068539990984000d913937b
    Signed-off-by: Marcin Sobczyk <msobczyk@redhat.com>

---
+++ mk/engine-installed.mk
@@ -0,0 +1,14 @@
+%-provision-engine.sh:
+	cp $(PROVISION_ENGINE_SCRIPT) $@
+
+%-engine-installed.qcow2: %-upgrade.qcow2 %-provision-engine.sh
+	qemu-img create -f qcow2 -F qcow2 -b $*-upgrade.qcow2 $@.tmp
+#	See the remark above about chmod.
+	chmod 666 $@.tmp
+	virt-customize \
+		-a $@.tmp \
+		$(foreach repo, $(EXTRA_REPOS), --run-command "dnf config-manager --add-repo $(repo)") \
+		--run "$*-provision-engine.sh" \
+		--run-command "rpm -qa | sort > $(_PKGLIST_PATH)/$(@:.qcow2=-pkglist.txt)" \
+		--selinux-relabel
+	mv $@.tmp $@
---
+++ mk/host-installed.mk
@@ -0,0 +1,14 @@
+%-provision-host.sh:
+	cp $(PROVISION_HOST_SCRIPT) $@
+
+%-host-installed.qcow2: %-upgrade.qcow2 %-provision-host.sh
+	qemu-img create -f qcow2 -F qcow2 -b $*-upgrade.qcow2 $@.tmp
+#	See the remark above aboud chmod.
+	chmod 666 $@.tmp
+	virt-customize \
+		-a $@.tmp \
+		$(foreach repo, $(EXTRA_REPOS), --run-command "dnf config-manager --add-repo $(repo)") \
+		--run "$*-provision-host.sh" \
+		--run-command "rpm -qa | sort > $(_PKGLIST_PATH)/$(@:.qcow2=-pkglist.txt)" \
+		--selinux-relabel
+	mv $@.tmp $@
