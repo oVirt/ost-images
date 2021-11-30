d2313d236da5ec69cf5f156ea91a00ff467205fd
Michal Skrivanek <michal.skrivanek@redhat.com>
2021-11-30 09:50:47 +0100

    build: improve build.sh helper script
    The helper heavily depends on other variables being set, it's supposed
    to be used by jenkins automation.
    
    Change-Id: Iba2fb8b84905668ca6028a59b5a4ddb635cc1b75
    Signed-off-by: Michal Skrivanek <michal.skrivanek@redhat.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,48 @@
-#!/bin/bash -xe
+#!/bin/bash
+
+# This helper script builds ost-images.
+# It caches images in current directory, source code is supposed to be in ost-images subdir
+# Additional mandatory variables that need to be set:
+# DISTRO - which image to build. Defaults to el8stream.
+# BUILD_HE_INSTALLED - build appliance image. set to 0 or "" if you do not want it to be build. Not relevant for "node" distros. Defaults to 1.
+# NODE_URL_BASE - URL for ovirt-node/rhvh images - defaults to ovirt-node repo
+# CENTOS_CACHE_URL - URL we download isos from. If not provided the iso files must exist locally.
+# RHEL8 - RHEL 8 compose repo, must have BaseOS/x86_64/os/images subdir with bootable image. Used for rhel8 distro.
+# RHEL8_BUILD - RHV build repo. Used for rhel8 distro.
+# RHVM_REPO - repo for additional RHV packages like rhvm-appliance. Used only for rhvh distro.
+
+[[ -d ost-images ]] || { echo "missing ost-images subdir"; exit 1; }
+
+echo Building distro ${DISTRO:=el8stream}
+echo "with appliance: ${BUILD_HE_INSTALLED:=1}"
+echo "with node image url: ${NODE_URL_BASE:=https://resources.ovirt.org/repos/ovirt/tested/master/iso/ovirt-node-ng-installer/}"
+
+# cache CentOS images
+[[ -n "${CENTOS_CACHE_URL}" ]] && for i in CentOS.iso CentOS-Stream.iso CentOS-Stream-9.iso; do
+    curl $([[ -f $i ]] && echo -z $i) --fail --limit-rate 100M -O ${CENTOS_CACHE_URL}/$i || { echo Download of $i failed; rm -f $i; exit 1; }
+done
+# cache RHVH image
+if [ $DISTRO = "rhvh" ]; then
+    NODE_IMG=rhvh.iso
+    OPENSCAP_PROFILE='%addon org_fedora_oscap\ncontent-type = scap-security-guide\nprofile = xccdf_org.ssgproject.content_profile_rhvh-stig\n%end'
+    LATEST=$(curl --fail ${NODE_URL_BASE} | grep 'dvd1.iso<' | sed -n 's;.*>\(.*\)<.*;\1;p')
+    curl --fail -L -o $NODE_IMG $([[ -f $NODE_IMG ]] && echo -z $NODE_IMG) "${NODE_URL_BASE}/${LATEST}" || exit 1
+elif [ $DISTRO = "node" ]; then
+    NODE_IMG=node.iso
+    # Latest ovirt-node as built by https://jenkins.ovirt.org/job/ovirt-node-ng-image_master_build-artifacts-el8-x86_64
+    NODE_URL_DIST=el8
+    NODE_URL_LATEST_VERSION=$(curl --fail "${NODE_URL_BASE}" | sed -n 's;.*a href="\([0-9.-]*\)/.*;\1;p' | sort | tail -1)
+    curl --fail -L -o $NODE_IMG $([[ -f $NODE_IMG ]] && echo -z $NODE_IMG) ${NODE_URL_BASE}${NODE_URL_LATEST_VERSION}/${NODE_URL_DIST}/ovirt-node-ng-installer-${NODE_URL_LATEST_VERSION}.${NODE_URL_DIST}.iso || exit 1
+elif [ $DISTRO = "rhel8" ]; then
+    OPENSCAP_PROFILE='%addon org_fedora_oscap\ncontent-type = scap-security-guide\nprofile = xccdf_org.ssgproject.content_profile_stig\n%end'
+    for i in rhel8-provision-engine.sh.in rhel8-provision-host.sh.in; do
+        sed "s|%BUILD%|$RHEL8_BUILD|g" $i.in > $i
+    done
+fi
+
+pushd ost-images
+rm -rf rpmbuild/RPMS/*
+[ -d /var/tmp ] && export TMPDIR=/var/tmp #virt-sparsify
 
 autoreconf -if
 
@@ -8,4 +52,69 @@ sysconfdir=/etc
 localstatedir=/var
 ./configure --prefix=$prefix --libdir=$libdir --sysconfdir=$sysconfdir --localstatedir=$localstatedir
 
-make -e rpm
+TRIES=2
+while [ $TRIES -gt 0 ]; do #try again once
+  make DISTRO=$DISTRO clean
+  BUILD_WHAT="BUILD_BASE=1 BUILD_HOST_INSTALLED=1 BUILD_ENGINE_INSTALLED=1 BUILD_HE_INSTALLED=${BUILD_HE_INSTALLED}"
+  if [ $DISTRO = "el8" ]; then
+    time make \
+        DISTRO=$DISTRO \
+        INSTALL_URL=../CentOS.iso \
+        BUILD_BASE=1 \
+        BUILD_HOST_INSTALLED=1 \
+        BUILD_ENGINE_INSTALLED=1 \
+        BUILD_HE_INSTALLED=${BUILD_HE_INSTALLED} \
+        rpm
+  elif [ $DISTRO = "el8stream" ]; then
+    time make \
+        DISTRO=$DISTRO \
+        REPO_ROOT=http://mirror.centos.org/centos/8-stream \
+        INSTALL_URL=../CentOS-Stream.iso \
+        BUILD_BASE=1 \
+        BUILD_HOST_INSTALLED=1 \
+        BUILD_ENGINE_INSTALLED=1 \
+        BUILD_HE_INSTALLED=${BUILD_HE_INSTALLED} \
+        rpm
+  elif [ $DISTRO = "el9stream" ]; then
+    time make \
+        DISTRO=$DISTRO \
+        REPO_ROOT=https://composes.stream.centos.org/production/latest-CentOS-Stream/compose/ \
+        INSTALL_URL=../CentOS-Stream-9.iso \
+        BUILD_BASE=1 \
+        BUILD_HOST_INSTALLED=1 \
+        BUILD_ENGINE_INSTALLED= \
+        BUILD_HE_INSTALLED= \
+        rpm
+  elif [ $DISTRO = "rhel8" ]; then
+    time make \
+        DISTRO=$DISTRO \
+        REPO_ROOT=${RHEL8} \
+        INSTALL_URL=${RHEL8}/BaseOS/x86_64/os/ \
+        BUILD_BASE=1 \
+        BUILD_HOST_INSTALLED=1 \
+        BUILD_ENGINE_INSTALLED=1 \
+        BUILD_HE_INSTALLED=${BUILD_HE_INSTALLED} \
+        OPENSCAP_PROFILE="${OPENSCAP_PROFILE}" \
+        rpm
+  elif [ $DISTRO = "node" -o $DISTRO = "rhvh" ]; then
+    # REPO_ROOT is just where "other stuff" like rhvm-appliance comes from. Used only for rhvh.
+    time make \
+        DISTRO=$DISTRO \
+        REPO_ROOT=${RHVM_REPO} \
+        INSTALL_URL=../$NODE_IMG \
+        SPARSIFY_BASE=no \
+        DISK_SIZE=80G \
+        BUILD_BASE=1 \
+        BUILD_ENGINE_INSTALLED= \
+        BUILD_HOST_INSTALLED= \
+        BUILD_HE_INSTALLED= \
+        OPENSCAP_PROFILE="${OPENSCAP_PROFILE}" \
+        rpm
+  fi
+  [ $? -eq 0 ] && break
+  let TRIES-=1
+  sleep 600
+done
+[ $TRIES -eq 0 ] && exit 1
+
+popd
