cd2b79e3b8bc907afa979b15c0dab2b57ef9ac5e
acernek <61460818+acernek@users.noreply.github.com>
2022-02-09 17:33:43 +0100

    Verify the cirros image before copying in (#12)

--- makefiles/engine-installed.mk
+++ makefiles/engine-installed.mk
@@ -1,7 +1,11 @@
 %-provision-engine.sh:
 	cp $(PROVISION_ENGINE_SCRIPT) $@
 
-%-engine-installed.qcow2: %-base.qcow2 %-provision-engine.sh
+cirros.img:
+	curl -L -o cirros.img $(CIRROS_URL)
+	qemu-img check -f qcow2 cirros.img
+
+%-engine-installed.qcow2: %-base.qcow2 %-provision-engine.sh cirros.img
 	qemu-img create -f qcow2 -F qcow2 -b $*-base.qcow2 $@.tmp
 #	See the remark above about chmod.
 	chmod 666 $@.tmp
@@ -11,7 +15,7 @@
 		$(_CHANGE_DNF_CACHE_TO_DEV_SHM) \
 		--run "$*-provision-engine.sh" \
 		$(_RESTORE_REGULAR_DNF_CACHE) \
-		--run-command "curl -L -o /var/tmp/cirros.img $(CIRROS_URL)" \
+		--copy-in cirros.img:/var/tmp \
 		--run-command "rpm -qa | sort > $(_PKGLIST_PATH)/$(@:.qcow2=-pkglist.txt)" \
 		--run-command "setfiles -F -m -v $(SE_CONTEXT) $(PARTITIONS)" \
 		--selinux-relabel
--- makefiles/he-installed.mk
+++ makefiles/he-installed.mk
@@ -1,7 +1,11 @@
 %-provision-he.sh:
 	cp $(PROVISION_HE_SCRIPT) $@
 
-%-he-installed.qcow2: %-host-installed.qcow2 %-provision-he.sh
+cirros.img:
+	curl -L -o cirros.img $(CIRROS_URL)
+	qemu-img check -f qcow2 cirros.img
+
+%-he-installed.qcow2: %-host-installed.qcow2 %-provision-he.sh cirros.img
 	qemu-img create -f qcow2 -F qcow2 -b $*-host-installed.qcow2 $@.tmp
 #	See the remark above about chmod.
 	chmod 666 $@.tmp
@@ -12,7 +16,7 @@
 		--run "$*-provision-he.sh" \
 		--run-command "dnf download --downloaddir /var/tmp sysstat lm_sensors-libs.x86_64" \
 		$(_RESTORE_REGULAR_DNF_CACHE) \
-		--run-command "curl -L -o /var/tmp/cirros.img $(CIRROS_URL)" \
+		--copy-in cirros.img:/var/tmp \
 		--run-command "rpm -qa | sort > $(_PKGLIST_PATH)/$(@:.qcow2=-pkglist.txt)" \
 		--run-command "setfiles -F -m -v $(SE_CONTEXT) $(PARTITIONS)" \
 		--selinux-relabel
