bd6a2f6b27d74c9b7222a94e774cdc82b5f377d6
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-02-10 08:32:47 +0100

    fix cirros.img ownership

--- makefiles/he-installed.mk
+++ makefiles/he-installed.mk
@@ -1,10 +1,6 @@
 %-provision-he.sh:
 	cp $(PROVISION_HE_SCRIPT) $@
 
-cirros.img:
-	curl -L -o cirros.img $(CIRROS_URL)
-	qemu-img check -f qcow2 cirros.img
-
 %-he-installed.qcow2: %-host-installed.qcow2 %-provision-he.sh cirros.img
 	qemu-img create -f qcow2 -F qcow2 -b $*-host-installed.qcow2 $@.tmp
 #	See the remark above about chmod.
@@ -16,7 +12,8 @@ cirros.img:
 		--run "$*-provision-he.sh" \
 		--run-command "dnf download --downloaddir /var/tmp sysstat lm_sensors-libs.x86_64" \
 		$(_RESTORE_REGULAR_DNF_CACHE) \
-		--copy-in cirros.img:/var/tmp \
+		--upload cirros.img:/var/tmp \
+		--run-command chown root:root /var/tmp/cirros.img \
 		--run-command "rpm -qa | sort > $(_PKGLIST_PATH)/$(@:.qcow2=-pkglist.txt)" \
 		--run-command "setfiles -F -m -v $(SE_CONTEXT) $(PARTITIONS)" \
 		--selinux-relabel
