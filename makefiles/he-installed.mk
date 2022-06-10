f5b1dfc599a03a2c761dd1b7b5f9fa30d1d4504a
Michal Skrivanek <michal.skrivanek@redhat.com>
2022-06-10 09:21:09 +0200

    el9: he: download el8 sysstat and lm_sensors packages
    right now we only have el8 engine even on el9 hosts

--- makefiles/he-installed.mk
+++ makefiles/he-installed.mk
@@ -5,12 +5,13 @@
 	qemu-img create -f qcow2 -F qcow2 -b $*-host-installed.qcow2 $@.tmp
 #	See the remark above about chmod.
 	chmod 666 $@.tmp
+# TODO download el8 version of sysstat/lm_sensors until we have el9 engine
 	virt-customize \
 		-a $@.tmp \
 		--memsize $(_MEMSIZE) \
 		$(_CHANGE_DNF_CACHE_TO_DEV_SHM) \
 		--run "$*-provision-he.sh" \
-		--run-command "dnf download --downloaddir /var/tmp sysstat lm_sensors-libs.x86_64" \
+		--run-command "dnf download --releasever=8 --downloaddir /var/tmp sysstat lm_sensors-libs.x86_64" \
 		$(_RESTORE_REGULAR_DNF_CACHE) \
 		--upload cirros.img:/var/tmp \
 		--run-command "chown root:root /var/tmp/cirros.img" \
