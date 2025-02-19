fd67ec9b7196d112ea61aeb62fa7cb26d5cfa74e
Jean-Louis Dupond <jean-louis@dupond.be>
2025-02-19 14:49:12 +0100

    make el9stream default
    Make CentOS Stream 9 the new default.
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # This helper script builds ost-images.
 # It caches images in current directory, source code is supposed to be in ost-images subdir
 # Additional mandatory variables that need to be set:
-# DISTRO - which image to build. Defaults to el8stream.
+# DISTRO - which image to build. Defaults to el9stream.
 # BUILD_HE_INSTALLED - build appliance image. set to 0 or "" if you do not want it to be build. Not relevant for "node" distros. Defaults to 1.
 # NODE_URL_BASE - URL for ovirt-node/rhvh images - defaults to ovirt-node repo
 # CENTOS_CACHE_URL - URL we download isos from. If not provided the iso files must exist locally.
@@ -14,7 +14,7 @@
 
 [[ -d ost-images ]] || { echo "missing ost-images subdir"; exit 1; }
 
-echo Building distro ${DISTRO:=el8stream}
+echo Building distro ${DISTRO:=el9stream}
 echo "with appliance: ${BUILD_HE_INSTALLED:=1}"
 echo "with node image url: ${NODE_URL_BASE:=https://resources.ovirt.org/repos/ovirt/github-ci/ovirt-node-ng-image/}"
 
@@ -24,7 +24,7 @@ ISO_URL[alma9]="Alma.iso"
 ISO_URL[el8]="CentOS.iso"
 ISO_URL[el8stream]="CentOS-Stream.iso"
 ISO_URL[el9stream]="CentOS-Stream-9.iso"
-ISO_URL[storage]="CentOS-Stream.iso"
+ISO_URL[storage]="CentOS-Stream-9.iso"
 IMAGE=${ISO_URL[$DISTRO]}
 if [[ -n "${CENTOS_CACHE_URL}" && -n "$IMAGE" ]]; then
     echo "cache $IMAGE"
@@ -39,8 +39,7 @@ if [ $DISTRO = "rhvh" ]; then
 elif [ $DISTRO = "node" -o $DISTRO = "el9node" ]; then
     NODE_IMG="${DISTRO}.iso"
     # Latest ovirt-node as built by https://github.com/oVirt/ovirt-node-ng-image/actions/workflows/build.yml
-    NODE_URL_DIST=el8
-    [ $DISTRO = "el9node" ] && NODE_URL_DIST=el9
+    NODE_URL_DIST=el9
     NODE_URL_LATEST_VERSION=$(curl --fail "${NODE_URL_BASE}" | sed -n 's;.*a href="\(ovirt-node-ng-installer-[0-9.-]*.'$NODE_URL_DIST'.iso\)\".*;\1;p' | grep "\.${NODE_URL_DIST}\." | sort | tail -1)
     echo "latest node ${NODE_URL_BASE}${NODE_URL_LATEST_VERSION}"
     curl --fail -L -o $NODE_IMG $([[ -f $NODE_IMG ]] && echo -z $NODE_IMG) ${NODE_URL_BASE}${NODE_URL_LATEST_VERSION} || exit 1
