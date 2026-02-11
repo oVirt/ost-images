c4d43fce7c80da6a054e4db17a74e6cd9cb05138
Jean-Louis Dupond <jean-louis@dupond.be>
2026-02-11 14:26:31 +0100

    Convert code to use kiwi
    Modify the whole build system to make use of kiwi instead of the 'old'
    system which booted vm's from ISO's and ran a kickstart script.
    This is much faster and better supported. And works perfectly on CentOS
    10. It also aligns with the way we build the ovirt-appliance.
    
    This commit includes:
    - Change CI to use kiwi
    - Drop unneeded caching of ISO files
    - Rework makefile to use kiwi
    - Add support for AlmaLinux/CentOS 10
    - Convert kickstart config into kiwi xml's
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- build.sh
+++ build.sh
@@ -1,52 +1,20 @@
 #!/bin/bash
 
 # This helper script builds ost-images.
-# It caches images in current directory, source code is supposed to be in ost-images subdir
+# Source code is supposed to be in ost-images subdir
 # Additional mandatory variables that need to be set:
-# DISTRO - which image to build. Defaults to el9stream.
+# DISTRO - which image to build. Defaults to centos9.
 # BUILD_HE_INSTALLED - build appliance image. set to 0 or "" if you do not want it to be build. Not relevant for "node" distros. Defaults to 1.
-# NODE_URL_BASE - URL for ovirt-node images - defaults to ovirt-node repo
-# CENTOS_CACHE_URL - URL we download isos from. If not provided the iso files must exist locally.
-# OPENSCAP_PROFILE - set a profile during installation. Defaults to xccdf_org.ssgproject.content_profile_stig on rhel8.
 
 [[ -d ost-images ]] || { echo "missing ost-images subdir"; exit 1; }
 
-echo Building distro ${DISTRO:=el9stream}
+echo Building distro ${DISTRO:=centos}
+echo "distro version: ${DISTRO_VERSION:=9}"
 echo "with appliance: ${BUILD_HE_INSTALLED:=1}"
-echo "with node image url: ${NODE_URL_BASE:=https://resources.ovirt.org/repos/ovirt/github-ci/ovirt-node-ng-image/}"
-
-# cache CentOS images
-declare -A ISO_URL
-ISO_URL[almalinux9]="AlmaLinux-9.iso"
-ISO_URL[el9stream]="CentOS-Stream-9.iso"
-ISO_URL[storage]="CentOS-Stream-9.iso"
-ISO_URL[el9snode]="CentOS-Stream-9.iso"
-IMAGE=${ISO_URL[$DISTRO]}
-if [[ -n "${CENTOS_CACHE_URL}" && -n "$IMAGE" ]]; then
-    echo "cache $IMAGE"
-    curl $([[ -f $IMAGE ]] && echo "-z $IMAGE") --fail -O ${CENTOS_CACHE_URL}/$IMAGE || { echo Download of $IMAGE failed; rm -f $IMAGE; exit 1; }
-fi
-
-# cache ovirt-node image
-if [[ $DISTRO = "node" || $DISTRO = "el9snode" ]]; then
-    NODE_IMG="${DISTRO}.iso"
-    # Latest ovirt-node as built by https://github.com/oVirt/ovirt-node-ng-image/actions/workflows/build.yml
-    NODE_URL_DIST=c9s
-    NODE_URL_LATEST_VERSION=$(curl --fail "${NODE_URL_BASE}" | sed -n 's;.*a href="\(ovirt-node-ng-installer-[0-9.-]*.'$NODE_URL_DIST'.iso\)\".*;\1;p' | grep "\.${NODE_URL_DIST}\." | sort | tail -1)
-    echo "latest node ${NODE_URL_BASE}${NODE_URL_LATEST_VERSION}"
-    curl --fail -L -o "$NODE_IMG" $([[ -f "$NODE_IMG" ]] && echo -z "$NODE_IMG") ${NODE_URL_BASE}${NODE_URL_LATEST_VERSION} || exit 1
-fi
-
-echo "Distro doesn't work with OpenSCAP profiles properly, ignoring"
-OPENSCAP_PROFILE=
 
 pushd ost-images
 
 rm -rf rpmbuild/RPMS/*
-[ -d /var/tmp ] && export TMPDIR=/var/tmp #virt-sparsify
-
-# load vars specific for distro
-source "configs/${DISTRO}/build.env"
 
 autoreconf -if
 
@@ -59,16 +27,14 @@ localstatedir=/var
     --libdir=$libdir \
     --sysconfdir=$sysconfdir \
     --localstatedir=$localstatedir \
-    --with-distro=$DISTRO
+    --with-distro=$DISTRO \
+    --with-distro-version=$DISTRO_VERSION
 
-TRIES=2
-while [ $TRIES -gt 0 ]; do #try again once
-  make clean
-  time make -e rpm
-  [ $? -eq 0 ] && break
-  let TRIES-=1
-  sleep 600
-done
-[ $TRIES -eq 0 ] && exit 1
+make clean
+time make -e rpm
+
+MAKE_EXIT=$?
 
 popd
+
+exit $MAKE_EXIT
--- makefiles/cirros.mk
+++
@@ -1,3 +0,0 @@
-cirros.img:
-	curl -L -o $@ $(CIRROS_URL)
-	qemu-img check -f qcow2 $@
